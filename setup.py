a1a3306deae60b5a81d34b17c9f3b84c1cb458b0
Ernesto Perez Amigo <eamigo@nauta.cu>
2017-10-08 18:16:57 -0400

    Fix problem with setup and final versions str

--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ import re
 
 from setuptools import setup
 
+
 _version_re = re.compile(r'VERSION\s+=\s+(.*)')
 
 with open('graphene_django_extras/__init__.py', 'rb') as f:
     version = ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1))
     version = ".".join([str(v) for v in version])
+    version = version.split('.final')[0] if 'final' in version else version
+
 
 def get_packages():
     return [dirpath
             for dirpath, dirnames, filenames in os.walk('graphene_django_extras')
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
 
 setup(
     name='graphene-django-extras',
