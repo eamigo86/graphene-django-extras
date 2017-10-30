c7e27f4be664360bd50b6332bd81e26934781d38
Ernesto Perez Amigo <eamigo@nauta.cu>
2017-10-30 11:40:02 -0400

    Updated version

--- graphene_django_extras/__init__.py
+++ graphene_django_extras/__init__.py
@@ -8,7 +8,7 @@ from .paginations import LimitOffsetGraphqlPagination, PageGraphqlPagination, Cu
 from .types import DjangoObjectType, DjangoInputObjectType, DjangoListObjectType
 from .subscriptions import *
 
-VERSION = (0, 1, 0, 'alpha', '9')
+VERSION = (0, 1, 0, 'alpha', '10')
 
 __version__ = get_version(VERSION)
 
