683dba6dd519bb8a5108ef1477aea9b323914c3b
David Regla <dreglad@gmail.com>
2018-07-20 23:28:07 -0500

    pin version of django-filter package
    django-filter should be pinned until this gets fixed: https://github.com/graphql-python/graphene-django/issues/464
    As in: https://github.com/graphql-python/graphene-django/commit/f0705e2c25a07024334ea810562d263496ebbd9e

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
 
     install_requires=[
         'graphene-django>=2.0',
-        'django-filter>=1.0.4',
+        'django-filter<2',
         'djangorestframework>=3.6.0'
     ],
     extras_require={
