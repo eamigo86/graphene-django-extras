bde24ae495b51206ae261462056c87abe9c957af
Nick Sandford <nick.sandford@gmail.com>
2018-11-29 10:23:59 +1100

    Don't require a specific version of django-filter

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         'graphql-core==2.0',
         'graphene==2.0.1',
         'graphene-django==2.0',
-        'django-filter==1.1.0',
+        'django-filter>=1.1.0',
         'djangorestframework>=3.6.0'
     ],
     extras_require={
