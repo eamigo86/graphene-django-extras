d38599b88a4537d92b562010ca4b7e56580f2e35
Ernesto <eamigop86@gmail.com>
2019-06-30 21:40:20 +0200

    Reorder imports and use Black code style. Updated version to 0.4.4

--- graphene_django_extras/directives/__init__.py
+++ graphene_django_extras/directives/__init__.py
@@ -1,59 +1,36 @@
 # -*- coding: utf-8 -*-
 from graphql.type.directives import specified_directives as default_directives
 
-from .list import ShuffleGraphQLDirective, SampleGraphQLDirective
-from .numbers import FloorGraphQLDirective, CeilGraphQLDirective
-from .string import (
-    DefaultGraphQLDirective,
-    Base64GraphQLDirective,
-    NumberGraphQLDirective,
-    CurrencyGraphQLDirective,
-    LowercaseGraphQLDirective,
-    UppercaseGraphQLDirective,
-    CapitalizeGraphQLDirective,
-    CamelCaseGraphQLDirective,
-    SnakeCaseGraphQLDirective,
-    KebabCaseGraphQLDirective,
-    SwapCaseGraphQLDirective,
-    StripGraphQLDirective,
-    TitleCaseGraphQLDirective,
-    CenterGraphQLDirective,
-    ReplaceGraphQLDirective,
-)
-
-try:
-    import dateutil  # noqa: 401
-    from .date import DateGraphQLDirective
-
-    date_directives = (DateGraphQLDirective,)
-    DATEUTIL_INSTALLED = True
-except ImportError:
-    date_directives = ()
-    DATEUTIL_INSTALLED = False
-
-
-list_directives = (ShuffleGraphQLDirective, SampleGraphQLDirective)
-numbers_directives = (FloorGraphQLDirective, CeilGraphQLDirective)
-string_directives = (
-    DefaultGraphQLDirective,
-    Base64GraphQLDirective,
-    NumberGraphQLDirective,
-    CurrencyGraphQLDirective,
-    LowercaseGraphQLDirective,
-    UppercaseGraphQLDirective,
-    CapitalizeGraphQLDirective,
-    CamelCaseGraphQLDirective,
-    SnakeCaseGraphQLDirective,
-    KebabCaseGraphQLDirective,
-    SwapCaseGraphQLDirective,
-    StripGraphQLDirective,
-    TitleCaseGraphQLDirective,
-    CenterGraphQLDirective,
-    ReplaceGraphQLDirective,
-)
+from .date import *
+from .list import *
+from .numbers import *
+from .string import *
 
 all_directives = (
-    date_directives + list_directives + numbers_directives + string_directives
+    # date
+    DateGraphQLDirective,
+    # list
+    ShuffleGraphQLDirective,
+    SampleGraphQLDirective,
+    # numbers
+    FloorGraphQLDirective,
+    CeilGraphQLDirective,
+    # string
+    DefaultGraphQLDirective,
+    Base64GraphQLDirective,
+    NumberGraphQLDirective,
+    CurrencyGraphQLDirective,
+    LowercaseGraphQLDirective,
+    UppercaseGraphQLDirective,
+    CapitalizeGraphQLDirective,
+    CamelCaseGraphQLDirective,
+    SnakeCaseGraphQLDirective,
+    KebabCaseGraphQLDirective,
+    SwapCaseGraphQLDirective,
+    StripGraphQLDirective,
+    TitleCaseGraphQLDirective,
+    CenterGraphQLDirective,
+    ReplaceGraphQLDirective,
 )
 
 
