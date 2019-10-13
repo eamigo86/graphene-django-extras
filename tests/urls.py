a11b222bdb8c043d7293450754103f61787be34a
Ernesto <eamigop86@gmail.com>
2019-10-13 14:25:03 +0200

    Added support for Telegram notification

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,19 @@
+import django
 from django.contrib import admin
-from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
-]
+if django.VERSION >= (2, 0):
+    from django.urls import path
+
+    urlpatterns = [
+        path("admin/", admin.site.urls),
+        path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
+    ]
+else:
+    from django.urls import url
+
+    urlpatterns = [
+        url("admin/", admin.site.urls),
+        url("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql"),
+    ]
