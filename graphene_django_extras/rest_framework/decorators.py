a63733a82cb50ea9dc93ae9e0e63fb4bd325167d
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2020-05-27 23:37:26 +0100

    Update decorators.py

--- graphene_django_extras/rest_framework/decorators.py
+++ graphene_django_extras/rest_framework/decorators.py
@@ -1,20 +1,25 @@
 from functools import wraps
+
+from django.db.models import QuerySet
+from graphene_django.utils import maybe_queryset
 from graphql import GraphQLError
 
 from .mixins import GraphqlPermissionMixin
 
 __all__ = [
-    "login_required",
     "permission_required",
+    "is_super_user_required_msg",
+    "is_staff_required_msg",
+    "login_required_msg",
     "is_super_user_required",
     "is_staff_required",
+    "login_required",
 ]
 
 
-def login_required(not_auth_msg=None):
+def login_required_msg(not_auth_msg):
     """
     :param not_auth_msg: not authorization message
-    :return: f
     """
     def decorator(f):
         @wraps(f)
@@ -22,34 +27,41 @@ def login_required(not_auth_msg=None):
             root, info = args
             if info.context.user.is_authenticated:
                 return f(*args, **kwargs)
-            raise GraphQLError(not_auth_msg or 'Authentication is required')
+            raise GraphQLError(not_auth_msg)
+
         return wrap
+
     return decorator
 
 
-def permission_required(permissions=None):
+def permission_required(permission_classes):
     """
-    :param permissions: list of permission_classes
-    :return:
+    :param permission_classes: list of permission_classes
     """
+    assert isinstance(permission_classes, (list, tuple)), 'permission_classes must be instance of `list` or `tuple` '
     wrap_permission_mixin = GraphqlPermissionMixin()
-    wrap_permission_mixin.permission_classes = permissions or []
+    wrap_permission_mixin.permission_classes = permission_classes
 
     def decorator(f):
         @wraps(f)
         def wrap(*args, **kwargs):
             root, info = args
             wrap_permission_mixin.check_permissions(info.context)
-            return f(wrap_permission_mixin, root=root, info=info, **kwargs)
+            result = f(*args, **kwargs)
+            qs = maybe_queryset(result)
+            if isinstance(qs, QuerySet) and qs.exists():
+                wrap_permission_mixin.check_object_permissions(info.context, qs.first())
+            elif isinstance(qs, (list, tuple)) and len(qs) > 0:
+                wrap_permission_mixin.check_object_permissions(info.context, qs[0])
+            return result
         return wrap
     return decorator
 
 
-def is_super_user_required(no_auth_msg=None, not_auth_msg=None):
+def is_super_user_required_msg(no_auth_msg, not_auth_msg=None):
     """
     :param no_auth_msg: No authentication message
     :param not_auth_msg: Not authorized message
-    :return: f
     """
 
     def decorator(f):
@@ -58,20 +70,20 @@ def is_super_user_required(no_auth_msg=None, not_auth_msg=None):
             root, info = args
             user = info.context.user
             if not user.is_authenticated:
-                raise GraphQLError(no_auth_msg or 'Authentication is required')
+                raise GraphQLError(no_auth_msg)
             if not user.is_superuser:
                 raise GraphQLError(not_auth_msg or 'Not Authorized')
             return f(*args, **kwargs)
 
         return wrap
+
     return decorator
 
 
-def is_staff_required(no_auth_msg=None, not_auth_msg=None):
+def is_staff_required_msg(no_auth_msg, not_auth_msg=None):
     """
     :param no_auth_msg: No authentication message
     :param not_auth_msg: Not authorized message
-    :return: f
     """
 
     def decorator(f):
@@ -80,9 +92,16 @@ def is_staff_required(no_auth_msg=None, not_auth_msg=None):
             root, info = args
             user = info.context.user
             if not user.is_authenticated:
-                raise GraphQLError(no_auth_msg or'Authentication is required')
+                raise GraphQLError(no_auth_msg)
             if not user.is_superuser or not user.is_staff:
                 raise GraphQLError(not_auth_msg or 'Not Authorised')
             return f(*args, **kwargs)
+
         return wrap
+
     return decorator
+
+
+login_required = login_required_msg('Authentication is required')
+is_super_user_required = is_super_user_required_msg('Authentication is required')
+is_staff_required = is_staff_required_msg('Authentication is required')
