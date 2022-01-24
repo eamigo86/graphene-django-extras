1c194e54678c60e482d78ab2bf524fa9b41352aa
Keith Hackbarth <keith.hackbarth@gmail.com>
2022-01-23 20:24:00 -0800

    Subcribe is moved from executor to its own directory

--- graphene_django_extras/views.py
+++ graphene_django_extras/views.py
@@ -5,7 +5,7 @@ from django.core.cache import caches
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from graphql import Source, parse, execute
-from graphql.execution.executor import subscribe
+from graphql.subscription import subscribe
 from graphql.utils.get_operation_ast import get_operation_ast
 from rest_framework.decorators import (
     authentication_classes,
