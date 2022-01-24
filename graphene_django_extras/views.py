b48a9b103ac487fb40ab249a4ca674a667d6ff8b
Keith Hackbarth <keith.hackbarth@gmail.com>
2022-01-23 20:27:47 -0800

    graphql.utils is now graphql.utilities

--- graphene_django_extras/views.py
+++ graphene_django_extras/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from graphql import Source, parse, execute
 from graphql.subscription import subscribe
-from graphql.utils.get_operation_ast import get_operation_ast
+from graphql.utilities import get_operation_ast
 from rest_framework.decorators import (
     authentication_classes,
     permission_classes,
